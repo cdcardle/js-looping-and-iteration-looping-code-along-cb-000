2551caf006b76efc8ec6dfa28b2efaf8c6b8af90
Chris Cardle <cdcardle@gmail.com>
2018-10-04 18:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function printBadges(array) {
-  for (let i = 0, i < array.length, i++) {
-    console.log(`Welcome, ${array[i]}! You are employee #${i + 1}.`)
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Welcome, ${array[i]}! You are employee #${i + 1}.`);
   }
 }
